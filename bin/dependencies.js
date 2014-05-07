272b94e294e2937ec71392f33ecb06c969cc5d8e
Boyan Rabchev <boyan@rabchev.com>
2014-05-07 14:35:03 +0300

    Fixed shebang problem (cr/lf) introduced in the previous commit.

--- bin/dependencies.js
+++ bin/dependencies.js
@@ -1,5 +1,4 @@
 /*jslint plusplus: true, devel: true, nomen: true, node: true, vars: true, indent: 4, maxerr: 50 */
-/*global  */
 
 "use strict";
 
@@ -9,8 +8,8 @@ if (process.getuid && process.setuid) {
     exec = require("child_process").exec;
     exec("./bin/libpam-dev-check.sh", function (err) {
         if (!err) {
-            exec("npm install authenticate-pam", function (err) {
-                
+            exec("npm install authenticate-pam", function () {
+
             });
         } else {
             console.log(err);
--- bin/run.js
+++ bin/run.js
@@ -1,22 +1,19 @@
 #!/usr/bin/env node
 
 /*jslint plusplus: true, devel: true, nomen: true, node: true, vars: true, indent: 4, maxerr: 50 */
-/*global require, exports, module */
 
-var connect     = require("connect"),
-    util        = require("util"),
-    commander   = require("commander"),
+var commander   = require("commander"),
     terminal    = require("../"),
     pkg         = require("../package.json");
 
 if (commander.options.length === 0) {
     commander
-        .version(pkg.version)
-        .option("-p, --port <port>", "Cpecifies the TCP port for the HTTP server.")
-        .option("-s, --ssl", "Start HTTP server over secure socket layer.")
-        .option("-h, --shell <shell>", "Executes commands in the specified command shell. Example: --shell bash")
-        .option("-l, --login", "Require login to use the terminal. The process is executed with the logged user account. NOTE: this option works only for POSIX platforms and it requires libpam-dev package to be installed prior to installing web-terminal.")
-        .parse(process.argv);
+    .version(pkg.version)
+    .option("-p, --port <port>", "Cpecifies the TCP port for the HTTP server.")
+    .option("-s, --ssl", "Start HTTP server over secure socket layer.")
+    .option("-h, --shell <shell>", "Executes commands in the specified command shell. Example: --shell bash")
+    .option("-l, --login", "Require login to use the terminal. The process is executed with the logged user account. NOTE: this option works only for POSIX platforms and it requires libpam-dev package to be installed prior to installing web-terminal.")
+    .parse(process.argv);
 }
 
 if (commander.shell) {
