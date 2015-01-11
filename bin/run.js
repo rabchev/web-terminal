a41cf3995a06ca13e8667fe108a9c0ed6435bac0
Dennis Bartlett <bartlett.dc.1@gmail.com>
2015-01-11 01:47:47 -0600

    Altered to support passing in a host as option.  Made some small imporvements on code.

--- bin/run.js
+++ bin/run.js
@@ -9,7 +9,8 @@ var commander   = require("commander"),
 if (commander.options.length === 0) {
     commander
     .version(pkg.version)
-    .option("-p, --port <port>", "Cpecifies the TCP port for the HTTP server.")
+    .option("-i, --interface <ip>", "Interface (ip) to listen on.")
+    .option("-p, --port <port>", "Specifies the TCP port for the HTTP server.")
     .option("-s, --ssl", "Start HTTP server over secure socket layer.")
     .option("-h, --shell <shell>", "Executes commands in the specified command shell. Example: --shell bash")
     .option("-l, --login", "Require login to use the terminal. The process is executed with the logged user account. NOTE: this option works only for POSIX platforms and it requires libpam-dev package to be installed prior to installing web-terminal.")
@@ -24,4 +25,8 @@ if (commander.login) {
     process.env.WEBT_LOGIN = commander.login;
 }
 
-terminal(commander.port);
+terminal({
+    port: commander.port,
+    interface: commander.interface,
+    ssl: commander.ssl, 
+});
